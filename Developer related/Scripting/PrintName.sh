9a86eec0bad47dd83e4a466aeb93f65e9595d564
Ram11Coder <rambeliver11@gmail.com>
2021-12-03 22:55:52 +0530

    Added bash scripting

---
+++ Developer related/Scripting/PrintName.sh
@@ -0,0 +1,7 @@
+#This is a basic shell script command 
+#
+#Author : Thulasirman
+echo "Enter your name :"
+read name
+echo "Welcome !! $name"
+
---
+++ Developer related/Scripting/pattern1.sh
@@ -0,0 +1,11 @@
+
+echo "Enter the size"
+read num
+for ((i=1;i<=$num ;i++ ))
+do
+	for ((j=1;j<=$num;j++))
+	do
+echo -ne "* "
+done
+echo
+done
